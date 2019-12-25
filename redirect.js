ac8a0266459875768955c71cde51c4a478c68a79
Ilia Grigorev <Ilia.Grigorev@t-systems.com>
2019-12-25 15:41:54 +0300

    improve redirection

--- redirect.js
+++ redirect.js
@@ -1,13 +1,29 @@
 function globalURL(requestDetails) {
     document.cookie = "intl_locale=en_US;domain=.aliexpress.com;path=/ ";
-    let requestedUrl = requestDetails.url;
-    if (requestedUrl.startsWith("https://aliexpress.ru")) {
-        let globalSite = requestedUrl.replace("https://aliexpress.ru", "https://www.aliexpress.com");
+    document.cookie = "intl_locale=en_US;domain=.aliexpress.ru;path=/ ";
+    let requestUrl = requestDetails.url;
+    if (requestUrl.startsWith("https://aliexpress.ru")) {
+        let globalSite = requestUrl.replace("https://aliexpress.ru", "https://www.aliexpress.com");
         return {
             redirectUrl: globalSite
         };
-    } else if (requestedUrl.startsWith("https://ru.aliexpress.com")) {
-        let globalSite = requestedUrl.replace("https://ru.aliexpress.com", "https://www.aliexpress.com");
+    } else if (requestUrl.startsWith("https://ru.aliexpress.com")) {
+        let globalSite = requestUrl.replace("https://ru.aliexpress.com", "https://www.aliexpress.com");
+        return {
+            redirectUrl: globalSite
+        };
+    } else if (requestUrl.startsWith("https://www.aliexpress.com/ru/")) {
+        let globalSite = requestUrl.replace("https://www.aliexpress.com/ru/", "https://www.aliexpress.com/");
+        return {
+            redirectUrl: globalSite
+        };
+    } else if (requestUrl.startsWith("https://login.aliexpress.ru")) {
+        let globalSite = requestUrl.replace(/\.aliexpress\.ru/g, ".aliexpress.com");
+        return {
+            redirectUrl: globalSite
+        };
+    } else if (requestUrl.startsWith("https://trade.aliexpress.ru")) {
+        let globalSite = requestUrl.replace("https://trade.aliexpress.ru", "https://trade.aliexpress.com");
         return {
             redirectUrl: globalSite
         };
@@ -15,6 +31,25 @@ function globalURL(requestDetails) {
 
 }
 
+function globalURLHeaders(requestDetails) {
+    for (let header of requestDetails.requestHeaders) {
+        if (header.name.toLowerCase() === "cookie") {
+            header.value = header.value.replace(/locale\=ru\_RU/g, "locale=en_US").replace(/site\=rus/g, "site=glo");
+        }
+    }
+    for (let header of requestDetails.requestHeaders) {
+        if (header.name.toLowerCase() === "cookie") {
+            console.log(header.value);
+        }
+    }
+}
+
 browser.webRequest.onBeforeRequest.addListener(
-    globalURL, {urls: ["https://aliexpress.ru/*", "https://ru.aliexpress.com/*"]}, ["blocking"]
-);
+    globalURL, {
+    urls: ["https://aliexpress.ru/*", "https://ru.aliexpress.com/*", "https://www.aliexpress.com/ru/*", "https://login.aliexpress.ru/*"]
+}, ["blocking"]);
+
+browser.webRequest.onBeforeSendHeaders.addListener(
+    globalURLHeaders, {
+    urls: ["https://aliexpress.ru/*", "https://ru.aliexpress.com/*", "https://www.aliexpress.com/ru/*", "https://login.aliexpress.ru/*"]
+}, ["blocking", "requestHeaders"]);
