fbd8922836e75987d9520d15fe81ea71c0439ca9
Ilia Grigorev <Ilia.Grigorev@t-systems.com>
2020-09-04 12:31:53 +0300

    Yet another fix for endless redirections of store pages

--- redirect.js
+++ redirect.js
@@ -1,10 +1,12 @@
 const requiredUrls = ["*://aliexpress.ru/*", "*://aliexpress.com/*", "*://*.aliexpress.com/*", "*://*.aliexpress.ru/*"];
+const blockedUrls = ["*://*.mmstat.com/*", "*://mmstat.com/*"];
 // Currently it seems to be impossible to redirect store pages :(
 const storeRegexp = new RegExp('^http(s)?\:\/\/([a-z0-9]+.)?aliexpress\.ru\/store', 'i');
 
 const domainRegexp = new RegExp('^http(s)?\:\/\/([a-z0-9]+.)?aliexpress\.ru', 'i');
 const subdomainRegexp = new RegExp('^http(s)?\:\/\/ru\.aliexpress\.com', 'i');
 const pathRegexp = new RegExp('^http(s)?\:\/\/([a-z].)?aliexpress\.com\/ru\//', 'i');
+const mmstatRegexp = new RegExp('^http(s)?\:\/\/([a-z].)?mmstat\.com\//', 'i');
 
 function globalURL(requestDetails) {
     var requestUrl = requestDetails.url;
@@ -32,24 +34,37 @@ function globalURL(requestDetails) {
 function globalURLReqHeaders(reqDetails) {
     for (let header of reqDetails.requestHeaders) {
         if (header.name === "Cookie") {
-            header.value = header.value.replace(/locale\=ru\_RU/g, "locale=en_US").replace(/site\=rus/g, "site=glo");
+            header.value = header.value.replace(/locale\=ru\_RU/g, "locale=en_US").replace(/site\=rus/g, "site=glo");//.replace(/region\=RU/g, "region=US");
         }
     }
     return {
         requestHeaders: reqDetails.requestHeaders
-    }
+    };
 }
 
 function globalURLRespHeaders(respDetails) {
     for (let header of respDetails.responseHeaders) {
         if (header.name == "set-cookie") {
             header.value = header.value.replace(/locale\=ru\_RU/g, "locale=en_US").replace(/site\=rus/g, "site=glo");
+        } else if (header.name == "location" && !storeRegexp.test(header.value)) {
+            header.value = header.value.replace(/aliexpress\.ru/g, "aliexpress.com");
         }
     }
     return {
         responseHeaders: respDetails.responseHeaders
-    }
+    };
 }
+
+function blockURL(requestDetails) {
+    return {
+        cancel: true
+    };
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+    blockURL, {
+    urls: blockedUrls
+}, ["blocking", "requestBody"]);
 
 browser.webRequest.onBeforeRequest.addListener(
     globalURL, {
