d05310ca6767a8cec0ed47b2e31dc0d2177e9283
Ilia Grigorev <Ilia.Grigorev@t-systems.com>
2022-03-17 10:21:49 +0300

    Remove redirections

--- redirect.js
+++ redirect.js
@@ -1,40 +1,10 @@
 const requiredUrls = ["*://aliexpress.ru/*", "*://aliexpress.com/*", "*://*.aliexpress.com/*", "*://*.aliexpress.ru/*"];
 const blockedUrls = ["*://*.mmstat.com/*", "*://mmstat.com/*"];
-// Currently it seems to be impossible to redirect store pages :(
-const storeRegexp = new RegExp('^http(s)?\:\/\/([a-z0-9]+.)?aliexpress\.ru\/store', 'i');
-
-const domainRegexp = new RegExp('^http(s)?\:\/\/([a-z0-9]+.)?aliexpress\.ru', 'i');
-const subdomainRegexp = new RegExp('^http(s)?\:\/\/ru\.aliexpress\.com', 'i');
-const pathRegexp = new RegExp('^http(s)?\:\/\/([a-z].)?aliexpress\.com\/ru\//', 'i');
-const mmstatRegexp = new RegExp('^http(s)?\:\/\/([a-z].)?mmstat\.com\//', 'i');
-
-function globalURL(requestDetails) {
-    var requestUrl = requestDetails.url;
-    if (storeRegexp.test(requestUrl)) {
-        return;
-    } else if (domainRegexp.test(requestUrl)) {
-        let globalSite = requestUrl.replace(/aliexpress\.ru/g, "aliexpress.com");
-        return {
-            redirectUrl: globalSite
-        };
-    } else if (subdomainRegexp.test(requestUrl)) {
-        let globalSite = requestUrl.replace("ru.aliexpress.com", "www.aliexpress.com");
-        return {
-            redirectUrl: globalSite
-        };
-    } else if (pathRegexp.test(requestUrl)) {
-        let globalSite = requestUrl.replace("aliexpress.com/ru/", "aliexpress.com/");
-        return {
-            redirectUrl: globalSite
-        };
-    }
-
-}
 
 function globalURLReqHeaders(reqDetails) {
     for (let header of reqDetails.requestHeaders) {
         if (header.name === "Cookie") {
-            header.value = header.value.replace(/locale\=ru\_RU/g, "locale=en_US").replace(/site\=rus/g, "site=glo");//.replace(/region\=RU/g, "region=US");
+            header.value = header.value.replace(/locale\=ru\_RU/g, "locale=en_US").replace(/site\=rus/g, "site=glo");
         }
     }
     return {
@@ -46,8 +16,6 @@ function globalURLRespHeaders(respDetails) {
     for (let header of respDetails.responseHeaders) {
         if (header.name == "set-cookie") {
             header.value = header.value.replace(/locale\=ru\_RU/g, "locale=en_US").replace(/site\=rus/g, "site=glo");
-        } else if (header.name == "location" && !storeRegexp.test(header.value)) {
-            header.value = header.value.replace(/aliexpress\.ru/g, "aliexpress.com");
         }
     }
     return {
@@ -64,11 +32,6 @@ function blockURL(requestDetails) {
 browser.webRequest.onBeforeRequest.addListener(
     blockURL, {
     urls: blockedUrls
-}, ["blocking", "requestBody"]);
-
-browser.webRequest.onBeforeRequest.addListener(
-    globalURL, {
-    urls: requiredUrls
 }, ["blocking", "requestBody"]);
 
 browser.webRequest.onBeforeSendHeaders.addListener(
